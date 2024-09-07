f57d8929bb1022b04de61274acb86e125527611c
bozotats <112152266+bozotats@users.noreply.github.com>
2024-09-06 23:31:06 -0700

    first commit

---
+++ ContarHtml.java
@@ -0,0 +1,15 @@
+//Programa para recibir un archivo, y un texto en los args, y contar cuantas veces aparece el texto en el archivo
+import javax.swing.*;
+import java.util.logging.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ContarHtml {
+    private static final Logger LOGGER = Logger.getLogger(ContarHtml.class.getName());
+
+    public static void main(String[] args) {
+        
+    }
+
+}
