8214cf0e40c34fb084cc950456bce4dd0f6b85c0
bozotats <112152266+bozotats@users.noreply.github.com>
2024-09-18 11:23:03 -0700

    parrafos taghandler

--- HTMLTagHandlerContar.java
+++ HTMLTagHandlerContar.java
@@ -1,7 +1,13 @@
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLEditorKit;
 import java.util.logging.Logger;
 
 public class HTMLTagHandlerContar extends HTMLEditorKit.ParserCallback {
+    
+    private boolean inParagraph;
+    private int contador;
+
     @Override
     public void handleText(char[] data, int pos) {
 
@@ -16,10 +22,27 @@ public class HTMLTagHandlerContar extends HTMLEditorKit.ParserCallback {
         }
 
         String texto = new String( data );
-        for (int i = -1; (i = texto.indexOf(keyword, i + 1)) != -1; i++) {
-            LOGGER.fine("Palabra clave encontrada en la posicion: " + i);
+        if (inParagraph) {
+            for (int i = -1; (i = texto.indexOf(keyword, i + 1)) != -1; i++) {
+                LOGGER.fine("Palabra clave encontrada en la posicion: " + i);
+            }
         }
+            
         
+    }
+
+    @Override
+    public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+        if( t == HTML.Tag.P ) {
+            inParagraph = true;
+        }
+    }
+
+    @Override
+    public void handleEndTag(HTML.Tag t, int pos) {
+        if( t == HTML.Tag.P ) {
+            inParagraph = false;
+        }
     }
 
     public String keyword = null;
