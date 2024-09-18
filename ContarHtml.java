b9f39e11ffd0f4860841c208f377f4119d6e8759
bozotats <112152266+bozotats@users.noreply.github.com>
2024-09-18 11:11:35 -0700

    procesar archivo html con parser

--- ContarHtml.java
+++ ContarHtml.java
@@ -1,5 +1,5 @@
 //Programa para recibir un archivo, y un texto en los args, y contar cuantas veces aparece el texto en el archivo
-import javax.swing.*;
+import javax.swing.text.html.*;
 import java.util.logging.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -21,7 +21,29 @@ public class ContarHtml {
 
         LOGGER.addHandler(archivo);
         LOGGER.setLevel(Level.ALL);
-        LOGGER.fine("hola mundo");
+        FileReader fReader = null;
+
+        try {
+            fReader = new FileReader(args[0]);
+        } catch (FileNotFoundException e) {
+            LOGGER.severe("No se puede abrir el archivo");
+            System.exit(1);
+        }
+
+        ContarParse parser = new ContarParse();
+        HTMLEditorKit.Parser procesador = parser.getParser();
+
+        try {
+            HTMLTagHandlerContar manejoEtiquetas = new HTMLTagHandlerContar();
+            manejoEtiquetas.keyword = args[1];
+            manejoEtiquetas.LOGGER = LOGGER;
+
+            procesador.parse(fReader, manejoEtiquetas, true);
+
+        } catch (IOException e) {
+            LOGGER.severe("No se puede leer el archivo");
+            System.exit(2);
+        }
 
     }
 
